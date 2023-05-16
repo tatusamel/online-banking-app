3633ad8d656c8c598ed047e859bcce04488f5298
tatusamel <55717583+tatusamel@users.noreply.github.com>
2023-05-16 21:16:05 +0300

    first commit

---
+++ online-banking-app/src/main/java/com/example/onlinebankingapp/OnlineBankingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinebankingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBankingAppApplication.class, args);
+    }
+
+}
---
+++ online-banking-app/src/test/java/com/example/onlinebankingapp/OnlineBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinebankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
