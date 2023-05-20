a2bbe944eb3aa2cd9533d3ae86e22b80058098a2
tatusamel <tatusamell@gmail.com>
2023-05-21 00:02:35 +0300

    fix missing imports

--- backend/src/main/java/com/example/onlinebankingapp/controller/AccountController.java
+++ backend/src/main/java/com/example/onlinebankingapp/controller/AccountController.java
@@ -6,6 +6,7 @@ import com.example.onlinebankingapp.service.AccountService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
